f95d5d604b7caa039552e40d83091bf030a43b99
shubambhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-12 19:11:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import "./styles.css";
+
+var bgcolor = "gray"
+var headingtext = "Lets DO some Magic"
+
+
+var emojiDictonary = {
+
+  "👿": "imp",
+  "😾": "Pouting Cat",
+  "😠": "angry",
+  "💔": "Broken Heart",
+  "😱": "Scream",
+  "😟": "worried",
+};
+
+
+
+
+
+export default function App() {
+
+  const [userInput, setUserInput] = useState("");
+  const [meaning, setMeaning] = useState("");
+
+
+  
+  inputEventHandler = (event) => {
+
+   var userInput = event.target.value
+
+   var emojiMeaning = emojiDictonary[userInput]; //thus meaning is not same as useState wala meaning
+
+   console.log(emojiMeaning);
+
+   setMeaning(emojiMeaning);
+
+   
+
+  }
+
+  return (
+    <div className="App">
+      
+      <h1>Inside Out</h1>
+      <input onChange={inputEventHandler}></input>
+      
+
+      <h3>Emoji Means: {meaning}</h3>
+
+
+      
+
+      
+
+
+     
+      
+      
+    </div>
+  );
+}
