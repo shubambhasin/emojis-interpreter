95c8cb3da0f33ddf5cd1ce9ca912155cf4dfe32c
shubambhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-12 23:23:02 +0530

    update: added more emojis

--- App.js
+++ App.js
@@ -6,14 +6,125 @@ var headingtext = "Lets DO some Magic"
 
 
 var emojiDictonary = {
-
-  "👿": "imp",
-  "😾": "Pouting Cat",
-  "😠": "angry",
-  "💔": "Broken Heart",
-  "😱": "Scream",
-  "😟": "worried",
-};
+  '☠️': "skull_and_crossbones"
+  ,'☹️': "frowning_face"
+  ,'☺️': "relaxed"
+  ,'✌️': "v"
+  ,'❤️': "heart"
+  ,'👄': "lips"
+  ,'👊': "punch"
+  ,'👌': "ok_hand"
+  ,'👍': "thumbsup"
+  ,'👎': "thumbsdown"
+  ,'👏': "clap"
+  ,'👻': "ghost"
+  ,'👿': "imp"
+  ,'💀': "skull"
+  ,'💋': "kiss"
+  ,'💓': "heartbeat"
+  ,'💔': "broken_heart"
+  ,'💕': "two_hearts"
+  ,'💖': "sparkling_heart"
+  ,'💗': "heartpulse"
+  ,'💘': "cupid"
+  ,'💙': "blue_heart"
+  ,'💚': "green_heart"
+  ,'💛': "yellow_heart"
+  ,'💜': "purple_heart"
+  ,'💝': "gift_heart"
+  ,'💞': "revolving_hearts"
+  ,'💩': "shit"
+  ,'💯': "100"
+  ,'🖕': "fu"
+  ,'🖤': "black_heart"
+  ,'😀': "grinning"
+  ,'😁': "grin"
+  ,'😂': "joy"
+  ,'😃': "smiley"
+  ,'😄': "smile"
+  ,'😅': "sweat_smile"
+  ,'😆': "laughing"
+  ,'😇': "innocent"
+  ,'😈': "smiling_imp"
+  ,'😉': "wink"
+  ,'😊': "blush"
+  ,'😋': "yum"
+  ,'😌': "relieved"
+  ,'😍': "heart_eyes"
+  ,'😎': "sunglasses"
+  ,'😏': "smirk"
+  ,'😐': "neutral_face"
+  ,'😑': "expressionless"
+  ,'😒': "unamused"
+  ,'😓': "sweat"
+  ,'😔': "pensive"
+  ,'😕': "confused"
+  ,'😖': "confounded"
+  ,'😗': "kissing"
+  ,'😘': "kissing_heart"
+  ,'😙': "kissing_smiling_eyes"
+  ,'😚': "kissing_closed_eyes"
+  ,'😛': "stuck_out_tongue"
+  ,'😜': "stuck_out_tongue_winking_eye"
+  ,'😝': "stuck_out_tongue_closed_eyes"
+  ,'😞': "disappointed"
+  ,'😟': "worried"
+  ,'😠': "angry"
+  ,'😡': "rage"
+  ,'😢': "cry"
+  ,'😣': "persevere"
+  ,'😤': "triumph"
+  ,'😥': "disappointed_relieved"
+  ,'😦': "frowning"
+  ,'😧': "anguished"
+  ,'😨': "fearful"
+  ,'😩': "weary"
+  ,'😪': "sleepy"
+  ,'😫': "tired_face"
+  ,'😬': "grimacing"
+  ,'😭': "sob"
+  ,'😮': "open_mouth"
+  ,'😯': "hushed"
+  ,'😰': "cold_sweat"
+  ,'😱': "scream"
+  ,'😲': "astonished"
+  ,'😳': "flushed"
+  ,'😴': "sleeping"
+  ,'😵': "dizzy_face"
+  ,'😶': "no_mouth"
+  ,'😷': "mask"
+  ,'😸': "smile_cat"
+  ,'😹': "joy_cat"
+  ,'😺': "smiley_cat"
+  ,'😻': "heart_eyes_cat"
+  ,'😼': "smirk_cat"
+  ,'😽': "kissing_cat"
+  ,'😾': "pouting_cat"
+  ,'😿': "crying_cat_face"
+  ,'🙀': "scream_cat"
+  ,'🙁': "slightly_frowning_face"
+  ,'🙂': "slightly_smiling_face"
+  ,'🙃': "upside_down_face"
+  ,'🙄': "roll_eyes"
+  ,'🙌': "raised_hands"
+  ,'🙏': "pray"
+  ,'🤐': "zipper_mouth_face"
+  ,'🤑': "money_mouth_face",
+  '🤒': "face_with_thermometer"
+  ,'🤓': "nerd_face"
+  ,'🤔': "thinking"
+  ,'🤕': "face_with_head_bandage"
+  ,'🤗': "hugs"
+  ,'🤝': "handshake"
+  ,'🤞': "crossed_fingers"
+  ,'🤠': "cowboy_hat_face"
+  ,'🤡': "clown_face"
+  ,'🤢': "nauseated_face"
+  ,'🤣': "rofl"
+  ,'🤤': "drooling_face"
+  ,'🤥': "lying_face"
+  ,'🤧': "sneezing_face"
+  }
 
 console.log(Object.keys(emojiDictonary))
 var emojisWeKnow = (Object.keys(emojiDictonary))
@@ -54,12 +165,13 @@ export default function App() {
   return (
     <div className="App">
       
-      <h1>Whats my Emoji 😷</h1>
+      <h1 className="heading">Whats my Emoji 😍</h1>
       <input onChange={inputEventHandler}></input>
       
-
-      <h3>Above  ☝️️ Emoji Means: {meaning}</h3>
-
+      <h1></h1>
+      <h3 className="heading2">Above  ☝️️ Emoji Means: {meaning}</h3>
+      
+      
       <h2> Emojis in Our Database 👇 :</h2>
 
       <h3>Clicked Emoji : {emojiName}</h3>
