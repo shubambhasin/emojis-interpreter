a9e59637b92e63130bfdcd22ce1b8c990f54c4e0
shubambhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-12 21:04:27 +0530

    feat: added search and interpretion
    Now you can search the meaning of the emojis

--- App.js
+++ App.js
@@ -1,38 +1,94 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./styles.css";
 
-var bgcolor = "gray";
-var headingtext = "Lets DO some Magic";
+var bgcolor = "gray"
+var headingtext = "Lets DO some Magic"
+
 
 var emojiDictonary = {
+
   "👿": "imp",
   "😾": "Pouting Cat",
   "😠": "angry",
   "💔": "Broken Heart",
   "😱": "Scream",
-  "😟": "worried"
+  "😟": "worried",
 };
 
+console.log(Object.keys(emojiDictonary))
+var emojisWeKnow = (Object.keys(emojiDictonary))
+
+
+
+
+
 export default function App() {
-  const [userInput, setUserInput] = useState("");
+
+
   const [meaning, setMeaning] = useState("");
 
+  
+
+  
   inputEventHandler = (event) => {
-    var userInput = event.target.value;
 
-    var emojiMeaning = emojiDictonary[userInput]; //thus meaning is not same as useState wala meaning
+   var userInput = event.target.value
 
-    console.log(emojiMeaning);
+   var emojiMeaning = emojiDictonary[userInput]; //thus meaning is not same as useState wala meaning
 
-    setMeaning(emojiMeaning);
-  };
+   console.log(emojiMeaning);
+
+   setMeaning(emojiMeaning);
+
+   
+
+  }
+
+  checkEmojiName = (emoji) =>
+  {
+    console.log(emoji)
+    setMeaning(emojiDictonary[emoji])
+  }
 
   return (
     <div className="App">
-      <h1>Inside Out</h1>
+      
+      <h1>Whats my Emoji 😷</h1>
       <input onChange={inputEventHandler}></input>
+      
 
-      <h3>Emoji Means: {meaning}</h3>
+      <h3>Above  ☝️️ Emoji Means: {meaning}</h3>
+
+      <h2> Emojis in Our Database 👇 :</h2>
+
+      <h3>Clicked Emoji : {meaning}</h3>
+
+      <div>
+        {
+        emojisWeKnow.map((emoji) => 
+        {
+
+          return(
+          <span 
+          onClick = {() => checkEmojiName(emoji)}  
+          className="big-emoji"
+          key={emoji}>
+          
+          {emoji}</span>)
+          }
+        )
+      }
+      </div>
+
+
+      
+
+      
+
+
+     
+      
+      
     </div>
   );
 }
